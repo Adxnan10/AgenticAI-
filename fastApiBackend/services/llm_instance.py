d0440a9dcb2766e0b0354f9a8300c1dac8fa42c0
Adxnan10 <Adnan66b@gmail.com>
2025-03-15 20:41:07 +0300

    instantiate an instance of LLaMA

---
+++ fastApiBackend/services/llm_instance.py
@@ -0,0 +1,8 @@
+from langchain_ollama import ChatOllama
+
+llm = ChatOllama(
+    model = "llama3.2",
+    temperature = 0.8,
+    num_predict = 256, # Max number of tokens to generate. I set it to 256 but that is not the limit
+)
+
