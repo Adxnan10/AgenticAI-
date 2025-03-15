0ff22e6ca000f48d2292c0a2252d84b1f7e62b26
Adxnan10 <Adnan66b@gmail.com>
2025-03-15 21:19:08 +0300

    add service: query router

--- fastApiBackend/services/llm_instance.py
+++ fastApiBackend/services/llm_instance.py
@@ -4,5 +4,6 @@ llm = ChatOllama(
     model = "llama3.2",
     temperature = 0.8,
     num_predict = 256, # Max number of tokens to generate. I set it to 256 but that is not the limit
+    format="json"
 )
 
